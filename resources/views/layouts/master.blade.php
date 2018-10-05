957aef713aa3e887c733c8105f6c031a0991b290
岸祐太 <kishiyuuta@mbp.local>
2018-10-05 21:59:08 +0900

    全体ヘッダー、フッダーblade作成

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+  <meta charset="utf-8">
+  <title>nyan | 猫になる</title>
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/style.css') }}">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/default.css') }}">
+  <link href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <h1 class="logo-wrapper">
+      <div class="left-space"></div>
+      <div class="center-logo"><a href="#"><img class="logo" src="./img/nyan.png" alt=""></a></div>
+      <div class="right-user"></div>
+    </h1>
+  </header>
+
+@yield('content')
+
+<footer>
+  <p><a href="#">利用規約</a>　｜　<a href="#">プライバシーポリシー</a></p>
+  <p>©irie development. All rights reserved.</p>
+</footer>
+
+</body>
+</html>
