bd05f2608f43c5056bd3c3ec2e200d6de988f399
岸祐太 <kishiyuuta@mbp.local>
2018-10-07 08:02:12 +0900

    header余白調整

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
 <body>
   <header>
     <h1 class="logo-wrapper">
-      <div class="left-space"></div>
+      <div class="left-space"> 　　</div>
       <div class="center-logo"><a href="#"><img class="logo" src="./img/nyan.png" alt=""></a></div>
       <div class="right-user"><a href="#"><img class="user-icon" src="{{ Auth::user()->avatar }}" alt=""></a>
     </h1>
