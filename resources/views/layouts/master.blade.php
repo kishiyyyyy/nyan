96929de25fac1e612ce677fc9c66e984db418fda
岸祐太 <kishiyuuta@mbp.local>
2018-10-07 07:53:36 +0900

    headerにTwitterAPIで取得したavatarを表示

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -16,7 +16,7 @@
     <h1 class="logo-wrapper">
       <div class="left-space"></div>
       <div class="center-logo"><a href="#"><img class="logo" src="./img/nyan.png" alt=""></a></div>
-      <div class="right-user"></div>
+      <div class="right-user"><a href="#"><img class="user-icon" src="{{ Auth::user()->avatar }}" alt=""></a>
     </h1>
   </header>
 
