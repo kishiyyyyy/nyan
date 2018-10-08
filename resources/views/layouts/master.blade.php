9a50dc02c9903249d27ee8f6d7e3c26b35c71ea1
岸祐太 <kishiyuuta@mbp.local>
2018-10-08 17:00:29 +0900

    favicon設定

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,6 +10,7 @@
   <link href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" rel="stylesheet">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <link rel="shortcut icon" type="image/x-icon" href="/img/favicon.ico">
 </head>
 
 <body>
