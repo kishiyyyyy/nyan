9696afb4863b7fce5e366a2f9ca305419aafc136
岸祐太 <kishiyuuta@mbp.local>
2018-11-19 06:37:20 +0900

    ユーザが猫の際はavatarを上書きしないよう

--- app/Http/Controllers/Auth/SocialAccountController.php
+++ app/Http/Controllers/Auth/SocialAccountController.php
@@ -39,13 +39,11 @@ class SocialAccountController extends Controller
                     'nickname' => $twitter_user->getNickname(),
                   ]);
             } else {
-                
-                $user->email = $twitter_user->getEmail();
-                $user->avatar = $twitter_user->avatar_original;
-                $user->name = $twitter_user->getName();
-                $user->nickname = $twitter_user->getNickname();
-
-                $user->save();
+                //ユーザが猫ではない時はavaterを上書きする
+                if (!($user->is_cat_flg)){
+                  $user->avatar = $twitter_user->avatar_original;
+                  $user->save();
+                }
 
             }
 
